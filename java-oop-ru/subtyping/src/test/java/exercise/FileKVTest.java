5b4dc5a4b74cdc068cd2743e6b58eacc9ea14ac5
@hexlet/cli <support@hexlet.io>
2024-10-11 19:02:47 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/test/java/exercise/FileKVTest.java
+++ java-oop-ru/subtyping/src/test/java/exercise/FileKVTest.java
@@ -12,7 +12,10 @@ import java.nio.file.StandardOpenOption;
 import com.fasterxml.jackson.databind.ObjectMapper;
 // BEGIN
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Map;
 // END
 
 
@@ -20,19 +23,47 @@ class FileKVTest {
 
     private static Path filepath = Paths.get("src/test/resources/file").toAbsolutePath().normalize();
 
+    private FileKV fileKV;
+
     @BeforeEach
     public void beforeEach() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         String content = mapper.writeValueAsString(new HashMap<String, String>());
         Files.writeString(filepath, content, StandardOpenOption.TRUNCATE_EXISTING);
+        fileKV = new FileKV(filepath.toString(), new HashMap<>());
     }
 
     // BEGIN
     @Test
-    public void getTest() {
-        KeyValueStorage actual = new FileKV("src/test/resources/file", Map.of("key", "value"));
-        var expected = "value";
-        assertEquals(expected, actual.get("key", "default"));
+    public void testSetAndGet() {
+        fileKV.set("key1", "value1");
+        assertEquals("value1", fileKV.get("key1", null));
+    }
+
+    @Test
+    public void testUnset() {
+        fileKV.set("key2", "value2");
+        fileKV.unset("key2");
+        assertEquals(null, fileKV.get("key2", null));
+    }
+
+    @Test
+    public void testDefaultValue() {
+        assertEquals("default", fileKV.get("nonExistentKey", "default"));
+    }
+
+    @Test
+    public void testToMap() {
+        fileKV.set("key3", "value3");
+        Map<String, String> map = fileKV.toMap();
+        assertEquals("value3", map.get("key3"));
+    }
+
+    @Test
+    public void testNewFileKV() {
+        fileKV.set("key4", "value4");
+        FileKV newFileKV = new FileKV(filepath.toString(), new HashMap<>());
+        assertEquals("value4", newFileKV.get("key4", null));
     }
     // END
 }
