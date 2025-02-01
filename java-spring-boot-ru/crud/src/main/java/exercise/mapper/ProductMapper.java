611819d05b7c1fcbf29d9de46416d3e3ae4b0ab5
@hexlet/cli <support@hexlet.io>
2025-02-01 21:28:34 +0300

    submit java-spring-boot-ru/crud

---
+++ java-spring-boot-ru/crud/src/main/java/exercise/mapper/ProductMapper.java
@@ -0,0 +1,25 @@
+package exercise.mapper;
+
+import exercise.dto.*;
+import exercise.model.Product;
+import org.mapstruct.*;
+
+// BEGIN
+@Mapper(
+        uses = {JsonNullableMapper.class, ReferenceMapper.class},
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class ProductMapper {
+    @Mapping(target = "category", source = "categoryId")
+    public abstract Product map(ProductCreateDTO dto);
+
+    @Mapping(source = "category.id", target = "categoryId")
+    @Mapping(source = "category.name", target = "categoryName")
+    public abstract ProductDTO map(Product model);
+
+    @Mapping(target = "category", source = "categoryId")
+    public abstract void update(ProductUpdateDTO dto, @MappingTarget Product model);
+}
+// END
